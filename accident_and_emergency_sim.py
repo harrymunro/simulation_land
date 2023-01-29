f29c39fdbb1c57426b019d617ad3bbc3509d1a7a
harrymunro <harry.j.munro@gmail.com>
2023-01-29 16:03:55 +0000

    Mods

--- accident_and_emergency_sim.py
+++ accident_and_emergency_sim.py
@@ -1,5 +1,4 @@
 import simpy
-import random
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -33,25 +32,30 @@ mean_monthly_attendence_per_person = december_2022_attendence / uk_population
 seconds_in_month = 60 * 60 * 24 * (365 / 12)
 
 # Data to save
-patient_wait_time_data = {"patient_name": [], "admittance_time":[], "wait_time": []}
-accident_and_emergency_queue_data = {"queue_length": [], "simulation_time": []}
+patient_wait_time_data = {"patient_name": [], "admittance_time_days":[], "wait_time_hrs": []}
+accident_and_emergency_queue_data = {"queue_length": [], "admittance_time_days": []}
 
-# A&E department component
-# Should delete patient when they leave hospital
 class AccidentEmergencyDepartment():
+
     def __init__(self, env, capacity):
         self.env = env
         self.attendance_capacity = simpy.Resource(env=env, capacity=capacity)
         self.waiting_room = []
 
     def process_patient(self, patient):
+        """
+        Process a patient.
+
+        Parameters:
+            patient (Patient): The patient to process.
+        """
         patient_arrival_time = self.env.now
         # patient waits in waiting room
         self.waiting_room.append(patient)
 
         # Good time now to record the accident and emergency queue length
         accident_and_emergency_queue_data["queue_length"].append(len(self.waiting_room))
-        accident_and_emergency_queue_data["simulation_time"].append(self.env.now)
+        accident_and_emergency_queue_data["admittance_time_days"].append(self.env.now/3600/24)
 
         # patient waits in attendance_capacity
         attendance_request = self.attendance_capacity.request()
@@ -64,15 +68,16 @@ class AccidentEmergencyDepartment():
         self.attendance_capacity.release(attendance_request)
         # patient is then discharged (we can add more complexity another for admissions)
         # patient is discharged
-        patient_finish_time = self.env.now - patient_arrival_time
+        patient_wait_time = self.env.now - patient_arrival_time
         #print(f"{patient.name} discharged from hospital after {patient_finish_time/3600} hours in A&E. Sim time {self.env.now/(3600*24)} days.")
         patient.live_life()
         # Save data
         patient_wait_time_data["patient_name"].append(patient.name)
-        patient_wait_time_data["admittance_time"].append(patient_arrival_time)
-        patient_wait_time_data["wait_time"].append(patient_finish_time)
+        patient_wait_time_data["admittance_time_days"].append(patient_arrival_time/3600/24)
+        patient_wait_time_data["wait_time_hrs"].append(patient_wait_time/3600)
 
-        print(len(self.waiting_room))
+
+
 
 class Person():
     def __init__(self, env, name, local_knowledge):
@@ -97,12 +102,12 @@ local_knowledge = {"nearest_hospital": None}
 env = simpy.Environment()
 
 # Create the A&E department
-accident_and_emergency_department = AccidentEmergencyDepartment(env=env, capacity=1)
+accident_and_emergency_department = AccidentEmergencyDepartment(env=env, capacity=2)
 
 local_knowledge["nearest_hospital"] = accident_and_emergency_department
 
 # Create the patients
-local_population_size = 10000
+local_population_size = 100000
 for person in range(local_population_size):
     name = names.get_full_name()
     #print(f"Creating {name}, person number {person} of {local_population_size}")
@@ -116,10 +121,12 @@ patient_wait_time_data = pd.DataFrame(patient_wait_time_data)
 accident_and_emergency_queue_data = pd.DataFrame(accident_and_emergency_queue_data)
 
 # Plot the data with seaborn
-sns.scatterplot(data=patient_wait_time_data, x="admittance_time", y="wait_time")
+sns.scatterplot(data=patient_wait_time_data, x="admittance_time_days", y="wait_time_hrs")
+plt.title("Patient wait times")
 plt.show()
 
-sns.scatterplot(data=accident_and_emergency_queue_data, x="simulation_time", y="queue_length")
+sns.scatterplot(data=accident_and_emergency_queue_data, x="admittance_time_days", y="queue_length")
+plt.title("Accident and emergency queue length")
 plt.show()
 
 
